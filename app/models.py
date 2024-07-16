182204938fb494855e44b7bb06a72fc2567b6509
Melki Mario Gulo <elhackerz.9je.19@gmail.com>
2024-07-16 09:09:39 +0700

    change models to add cascade

--- app/models.py
+++ app/models.py
@@ -4,14 +4,14 @@ from datetime import datetime
 class Kelas(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nama = db.Column(db.String(100), nullable=False)
-    siswas = db.relationship('Siswa', backref='kelas', lazy=True)
+    siswas = db.relationship('Siswa', backref='kelas', lazy=True, cascade='all, delete-orphan')
 
 class Siswa(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     absensi = db.Column(db.String(100), nullable=False)
     nama = db.Column(db.String(100), nullable=False)
     id_kelas = db.Column(db.Integer, db.ForeignKey('kelas.id'), nullable=False)
-    data_scans = db.relationship('DataScan', backref='siswa', lazy=True)
+    data_scans = db.relationship('DataScan', backref='siswa', lazy=True, cascade='all, delete-orphan')
 
 class DataScan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
